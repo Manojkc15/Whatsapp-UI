9c4a120092ee9357029dec2803457706496dc004
Manojkc15 <manojkc9845@gmail.com>
2021-08-29 23:56:52 +0530

    modified STATUS

--- lib/status.dart
+++ lib/status.dart
@@ -74,7 +74,52 @@ class Status extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(height: 20.0),
+          InkWell(
+            onTap: () {},
+            child: Container(
+              height: 60.0,
+              margin: EdgeInsets.all(12.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    child: Icon(
+                      Icons.person,
+                      size: 36.0,
+                      color: Colors.white,
+                    ),
+                    radius: 30.0,
+                    backgroundColor: Colors.blueGrey[300],
+                  ),
+                  SizedBox(width: 20.0),
+                  Expanded(
+                    flex: 4,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Name',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 7.0),
+                        Text(
+                          'Hi!',
+                          style: TextStyle(
+                            fontSize: 16.5,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           // ListView.builder(
           //   itemCount: 5,
           //   itemBuilder: (context, index) {
